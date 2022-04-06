9ae1fd5ac277815e31d39461258c26f71d0d9d5a
Mario Martínez Palacio <yonicstudios@gmail.com>
2022-04-06 21:27:26 +0200

    Update VISC rules and SWSH/VISC data

--- articles/visc/rules.js
+++ articles/visc/rules.js
@@ -1,6 +1,32 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import classNames from 'classnames';
 import styles from '../../styles/Article.module.css';
+
+const labels = {
+    confirmed: "Confirmed",
+    assumed: "Assumed",
+    possible: "Possible",
+    workaround: "Workaround",
+    no: "Not applicable",
+    unknown: "Unknown"
+}
+
+const classes = {
+    confirmed: styles.confirmed,
+    assumed: styles.guaranteed,
+    possible: styles.possible,
+    workaround: styles.workaround,
+    no: styles.no,
+    unknown: styles.unknown
+}
+
+const mapRows = (objeto) => (key) => (
+    <tr key={key}>
+        <td style={{textAlign: "center"}}>{key}</td>
+        <td className={classNames(styles.status, classes[objeto[key]])}>{labels[objeto[key]]}</td>
+    </tr>
+)
 
 export default function Rules() {
     return (
@@ -9,7 +35,7 @@ export default function Rules() {
                 <title>Rules for Pokémon Scarlet &amp; Violet - Billdex</title>
                 <meta name="description" content="This page shows the different rules for the tables and the current status of the selected criteria." />
             </Head>
-            <center><h1>Rules for Pokémon Scarlet &amp; Violet</h1></center>
+            <center><h1>Rules for <Link href="/visc"><a>Pokémon Scarlet &amp; Violet</a></Link></h1></center>
             <p>GameFreak has done it again, and will once more limit which Pokémon can be transfered to the new region.</p>
 
             <p>This page will hold an explanation for how the table works, and show the current state of the criteria taken under consideration to make this list.</p>
@@ -17,42 +43,7 @@ export default function Rules() {
             <p>Unlike the Sword &amp; Shield table, this table also keeps track of the different regional forms.</p>
 
             <p>There are five categories in this table. These are:</p>
-            <table>
-                <thead>
-                    <th>Category</th>
-                    <th>Description</th>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td style={{backgroundColor: "lime", fontWeight: "bold", textAlign:"center"}}>Confirmed</td>
-                        <td>These Pokémon have been confirmed to appear in the game and therefore transferable.</td>
-                    </tr>
-                    <tr>
-                        <td style={{backgroundColor: "blue", color:"white", fontWeight: "bold", textAlign:"center"}}>Guaranteed</td>
-                        <td>During Sword &amp; Shield's development, it was assumed that (in most scenarios) the rest of their evolutionary line would also appear.
-                            <br/>However, those games introduced region-exclusive evolutionary branches, putting this category in question. For Scarlet &amp; Violet, this category indicates that the likelihood
-                            for a Pokémon in this category is <i>very high</i>, even if the game doesn't launch with support from them right away.</td>
-                    </tr>
-                    <tr>
-                        <td style={{backgroundColor: "yellow", fontWeight: "bold", textAlign:"center"}}>Possible</td>
-                        <td>
-                            <p>These are Pokémon that may possibly appear. These Pokémon include:</p>
-                            <ul>
-                                <li>Pokémon that have appeared in-game as objects or other kind of representation, but not as a proper Pokémon.</li>
-                                <li>Pokémon that belong to an evolutionary branch that has an evolutionary requirement that do not meet the criteria for being guaranteed (see below).</li>
-                            </ul>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td style={{backgroundColor: "#222", color:"white", fontWeight: "bold", textAlign:"center"}}>Untransferable</td>
-                        <td>These Pokémon are <b>known</b> to not be able to be transfered to the new region.</td>
-                    </tr>
-                    <tr>
-                        <td style={{backgroundColor: "silver", fontWeight: "bold", textAlign:"center"}}>Unknown</td>
-                        <td>We currently don't have enough information to know if these Pokémon can be transfered.</td>
-                    </tr>
-                </tbody>
-            </table>
+            <CategoryTable />
 
             <h2>Criteria and assumptions</h2>
             <p>In order to classify all Pokémon in the list, a standard criteria is followed.
@@ -69,13 +60,198 @@ export default function Rules() {
                 <li>Some Pokémon may not be able to evolve.</li>
                 <li>New cross-generational Pokémon may appear (either breeding or evolution)</li>
             </ul>
-            <h3>Evolutions</h3>
 
+            <p>The following criteria are given a label to help classify criteria-sensitive cases.</p>
+            <CriteriaTable/>
+
+            <h3>Evolutions</h3>
             <p>Here is the current state of all evolution mechanics.</p>
+            <EvolutionTable/>
 
             <h3>Breeding</h3>
-
             <p>Here is the current state of all breeding mechanics.</p>
+            <BreedingTable/>
         </div>
+    )
+}
+
+const CategoryTable = () => (
+    <table className={styles.articleTable}>
+        <thead>
+            <tr>
+                <th>Category</th>
+                <th>Description</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td className={classNames(styles.status, styles.confirmed)}>Confirmed</td>
+                <td>These Pokémon have been confirmed to appear in the game and therefore transferable.</td>
+            </tr>
+            <tr>
+                <td className={classNames(styles.status, styles.guaranteed)}>Guaranteed</td>
+                <td>
+                    <p>During Sword &amp; Shield's development, it was assumed that (in most scenarios) the rest of their evolutionary line would also appear.</p>
+                    <p>However, those games introduced region-exclusive evolutionary branches, putting this category in question. For Scarlet &amp; Violet, this category indicates that the likelihood
+                    for a Pokémon in this category is <i>very high</i>, even if the game doesn't launch with support from them right away.</p>
+                </td>
+            </tr>
+            <tr>
+                <td className={classNames(styles.status, styles.possible)}>Possible</td>
+                <td>
+                    <p>These are Pokémon that may possibly appear. These Pokémon include:</p>
+                    <ul>
+                        <li>Pokémon that have appeared in-game as objects or other kind of representation, but not as a proper Pokémon.</li>
+                        <li>Pokémon that belong to an evolutionary branch that has an evolutionary requirement that do not meet the criteria for being guaranteed (see below).</li>
+                    </ul>
+                </td>
+            </tr>
+            <tr>
+                <td className={classNames(styles.status, styles.no)}>Untransferable</td>
+                <td>These Pokémon are <b>known</b> to not be able to be transfered to the new region.</td>
+            </tr>
+            <tr>
+                <td className={classNames(styles.status, styles.unknown)}>Unknown</td>
+                <td>We currently don't have enough information to know if these Pokémon can be transfered.</td>
+            </tr>
+        </tbody>
+    </table>
+);
+
+const CriteriaTable = () => (
+    <table style={{width: "50%"}} className={styles.articleTable}>
+        <thead>
+            <th>Label</th>
+            <th>Meaning</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td className={classNames(styles.status, styles.confirmed)}>Confirmed</td>
+                <td>This criteria is known to be applicable according to gameplay or official information.</td>
+            </tr>
+            <tr>
+                <td className={classNames(styles.status, styles.guaranteed)}>Assumed</td>
+                <td>According to the Pokémon that have been confirmed, it is assumed that this criteria is assumed to be applicable.</td>
+            </tr>
+            <tr>
+                <td className={classNames(styles.status, styles.possible)}>Possible</td>
+                <td>In theory, this criteria should be applicable. This label is used when Pokémon that use this criteria are confirmed.</td>
+            </tr>
+            <tr>
+                <td className={classNames(styles.status, styles.workaround)}>Replaced</td>
+                <td>A workaround exists for this criteria, which may affect how Pokémon can be classified.</td>
+            </tr>
+            <tr>
+                <td className={classNames(styles.status, styles.no)}>Not applicable</td>
+                <td>This criteria is known to be applicable according to gameplay or official information, but it doesn't mean that workarounds exist.</td>
+            </tr>
+            <tr>
+                <td className={classNames(styles.status, styles.unknown)}>Unknown</td>
+                <td>More information is needed to deliberate the status of this rule.</td>
+            </tr>
+        </tbody>
+    </table>
+);
+
+const EvolutionTable = () => {
+
+    const criteria = {
+        "Leveling up with a specific move": "possible",
+        "Leveling up in a specific location": "possible",
+        "Leveling up with a specific Pokémon": "unknown",
+        "Leveling up under a certain weather": "unknown",
+        "Trading with a specific item": "unknown",
+        "Trading with a specific Pokémon": "unknown",
+        "High friendship": "assumed",
+        "Dependance of time of day": "possible",
+        "Pokémon-specific evolution": "unknown",
+        "Dependance of regional form": "unknown",
+        "New evolution mechanics": "possible"
+    }
+
+    const evolutionaryItems = {
+        "Fire Stone": "unknown",
+        "Water Stone": "unknown",
+        "Thunder Stone": "possible",
+        "Leaf Stone": "unknown",
+        "Moon Stone": "unknown",
+        "Sun Stone": "possible",
+        "Shiny Stone": "unknown",
+        "Dusk Stone": "unknown",
+        "Dawn Stone": "unknown",
+        "Ice Stone": "unknown",
+        "King's Rock": "unknown",
+        "Metal Coat": "unknown",
+        "Dragon Scale": "unknown",
+        "Up-Grade": "unknown",
+        "Deep Sea Scale": "unknown",
+        "Deep Sea Tooth": "unknown",
+        "Dubious Disc": "unknown",
+        "Electirizer": "unknown",
+        "Magmarizer": "unknown",
+        "Protector": "unknown",
+        "Reaper Cloth": "unknown",
+        "Razor Claw": "unknown",
+        "Razor Fang": "unknown",
+        "Oval Stone": "unknown",
+        "Prism Scale": "unknown",
+        "Sachet": "unknown",
+        "Whipped dream": "unknown",
+        "Sweets": "unknown",
+        "Chipped & Cracked Pots": "unknown",
+        "Sweet & Tart Apples": "unknown",
+        "New evolutionary items": "possible"
+    }
+
+    return (
+        <table className={styles.articleTable}>
+            <thead>
+                <tr>
+                    <th>Criteria</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                {Object.keys(criteria).map(mapRows(criteria))}
+            </tbody>
+            <thead>
+                <tr>
+                    <th>Evolutionary item</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                {Object.keys(evolutionaryItems).map(mapRows(evolutionaryItems))}
+            </tbody>
+        </table>
+    )
+};
+
+const BreedingTable = () => {
+    const criteria = {
+        "Breeding": "possible",
+        "Leveling up in a specific location": "possible",
+        "Leveling up with a specific Pokémon": "unknown",
+        "Leveling up under a certain weather": "unknown",
+        "Trading with a specific item": "unknown",
+        "Trading with a specific Pokémon": "unknown",
+        "High friendship": "assumed",
+        "Dependance of time of day": "possible",
+        "Pokémon-specific evolution": "unknown",
+        "New evolution mechanics": "possible"
+    }
+
+    return (
+        <table className={styles.articleTable}>
+            <thead>
+                <tr>
+                    <th>Criteria</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                {Object.keys(criteria).map(mapRows(criteria))}
+            </tbody>
+        </table>
     )
 }
