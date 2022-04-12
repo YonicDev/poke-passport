0a56aba3b007942c363a0c2f6d182e88fc2d83e6
Mario Martínez Palacio <yonicstudios@gmail.com>
2022-04-12 21:45:47 +0200

    Set up configuration for Github Pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['raw.githubusercontent.com']
-  }
+  },
+  assetPrefix: isProd ? '/poke-passport/' : '',
 }
 
 module.exports = nextConfig
