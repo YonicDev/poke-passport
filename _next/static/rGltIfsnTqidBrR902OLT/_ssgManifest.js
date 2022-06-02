09fe5f5ef2cbf83063bdaedfe1d140c302f6399d
YonicDev <YonicDev@users.noreply.github.com>
2022-06-02 16:19:23 +0000

    deploy: 2b2ba9529c1f60c3796ef7d8ded6f3e20bbf9c0e

--- _next/static/rGltIfsnTqidBrR902OLT/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[game]\u002F[pokemon]","\u002F[game]\u002Farticle\u002F[article]","\u002F[game]\u002Fhistory","\u002F[game]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
