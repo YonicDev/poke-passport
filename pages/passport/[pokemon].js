b8b9427b0227b2aa653ad4fa91c464d9d4235d59
Mario Martínez Palacio <yonicstudios@gmail.com>
2022-06-02 15:05:12 +0200

    Passport: Support for regional forms

--- pages/passport/[pokemon].js
+++ pages/passport/[pokemon].js
@@ -1,3 +1,4 @@
+import { useReducer, useEffect } from 'react';
 import Head from 'next/head'
 import Link from 'next/link'
 import classNames from 'classnames';
@@ -9,8 +10,40 @@ import passportStyles from "../../styles/Passport.module.css";
 
 const latestGame = "visc";
 
+const regionRegExp = /(alola|galar|hisui)/;
+
 export default function PokemonPassport({pokemonData, prevPokemon, nextPokemon, pokemonNumber}) {
-    let thisPokemon = {...pokemonData};
+    const [selectedRegion, setSelectedRegion] = useReducer(() => {
+        const query = new URLSearchParams(window.location.search);
+        const region = query.get("region");
+        return regionRegExp.test(region)? region : "original";
+    }, "original");
+
+    useEffect(() => {
+        setSelectedRegion();
+    });
+
+    const isOriginal = selectedRegion == null || selectedRegion === "original";
+
+    let thisPokemon = !isOriginal && pokemonData[latestGame].forms!=null ? pokemonData[latestGame].forms.find(variant => {
+        const regexp = new RegExp(`-${selectedRegion}$`);
+        return regexp.test(variant.id);
+    })  : pokemonData;
+
+    if(thisPokemon==null) { thisPokemon = pokemonData; }
+
+    const formNames = {
+        "original": "Original",
+        "alola": "Alolan",
+        "galar": "Galarian",
+        "hisui": "Hisuian",
+    }
+    const regionShortForm = {
+        galar: {id: "swsh", size:"80px"},
+        alola: {id: "alola", size:"90px"},
+        original: {id: "visc", size:"90px"},
+        hisui: {id: "arceus", size:"90px"}
+    }
 
     const transferability = {
         swsh: {
@@ -38,32 +71,59 @@ export default function PokemonPassport({pokemonData, prevPokemon, nextPokemon, 
         return labels[pokemonData[game]?.status] === "Yes" ? acc + 1 : acc;
     }, 0);
 
+    const forms = pokemonData[latestGame].forms?.map(form => {
+        const formName = regionRegExp.exec(form.id)[1] // No need for validation. The regexp should always match.
+        if(formName===selectedRegion) return;
+        return (<div className={classNames(styles.formLink,passportStyles.default)} key={form.id}>
+            <Link href={`/passport/${pokemonData[latestGame].id}?region=${formName}`}><a title={`${formNames[formName]} form`}><img alt={form.id} src={`https://raw.githubusercontent.com/msikma/pokesprite/master/pokemon-gen8/regular/${form.id}.png`}/></a></Link>
+        </div>)
+    })
+
+    if(forms!=null && !isOriginal) {
+        forms.unshift(<div className={classNames(styles.formLink,passportStyles.default)} key={pokemonData[latestGame].id}>
+            <Link href={`/passport/${pokemonData[latestGame].id}`}><a title="Original form"><img alt={pokemonData[latestGame].id} src={`https://raw.githubusercontent.com/msikma/pokesprite/master/pokemon-gen8/regular/${pokemonData[latestGame].id}.png`}/></a></Link>
+        </div>)
+    }
+
     return (<div>
         <center>
             <h1>{pokemonData[latestGame].name}&apos;s Passport</h1>
-            <Link href={`/passport`}><a>↑ Back to list</a></Link>
+            <Link href={`/passport${!isOriginal?`?region=${selectedRegion}`:""}`}><a>↑ Back to list</a></Link>
         </center>
         <Head>
-            <title>Passport for {thisPokemon[latestGame].name} - PokéPassport</title>
-            <meta name="description" content={`Check which games ${thisPokemon.name} can be transfered to.`} />
+            <title>Passport for {pokemonData[latestGame].name} - PokéPassport</title>
+            <meta name="description" content={`Check which games ${pokemonData[latestGame].name} can be transfered to.`} />
         </Head>
         <div className={styles.pageContainer}>
-            <NavigationLink pokemon={prevPokemon} direction="left" number={pokemonNumber-1} />
+            <NavigationLink pokemon={prevPokemon} direction="left" number={pokemonNumber-1} preferredForm={selectedRegion}/>
             <div className={passportStyles.container}>
-                <div className={classNames(styles.iconContainer, passportStyles.default)} style={{backgroundImage: `url("/poke-passport/logo-visc.svg")`, backgroundSize: "90px", backgroundBlendMode: "multiply"}}>
-                    <img className={styles.icon} alt={pokemonData.name} src={`https://raw.githubusercontent.com/msikma/pokesprite/master/pokemon-gen8/regular/${thisPokemon[latestGame].id}.png`} />
+                <div className={classNames(styles.iconContainer, passportStyles.default)} style={{backgroundImage: `url("/poke-passport/logo-${regionShortForm[selectedRegion]?.id || regionShortForm.original.id}.svg")`, backgroundSize: regionShortForm[selectedRegion]?.size || regionShortForm.original.size, backgroundBlendMode: "multiply"}}>
+                    <img className={styles.icon} alt={pokemonData.name} src={`https://raw.githubusercontent.com/msikma/pokesprite/master/pokemon-gen8/regular/${isOriginal? pokemonData[latestGame].id : thisPokemon.id}.png`} />
                 </div>
-                <h1 className={styles.name}>#{addTrailingZeroes(pokemonNumber, 3)} {thisPokemon[latestGame].name}</h1>
+                <h1 className={styles.name}>#{addTrailingZeroes(pokemonNumber, 3)} {pokemonData[latestGame].name}</h1>
+                <div className={styles.forms}>{forms}</div>
                 <div className={passportStyles.label}>Passport Power<sup><abbr title="A rating based on how many games this Pokémon can be transfered to, excluding the games where a Pokémon first originated from.">(?)</abbr></sup>: {passportPower}</div>
                 <div className={passportStyles.separator}>Transferability</div>
-                <TransferabilityTable pokemon={thisPokemon} index={pokemonNumber} labels={transferability} />
+                <TransferabilityTable pokemon={pokemonData} index={pokemonNumber} labels={transferability} />
             </div>
-            <NavigationLink pokemon={nextPokemon} direction="right" number={pokemonNumber+1} />
+            <NavigationLink pokemon={nextPokemon} direction="right" number={pokemonNumber+1} preferredForm={selectedRegion}/>
         </div>
     </div>)
 }
 
 function TransferabilityTable({pokemon, labels, index}) {
+    const [region, setSelectedRegion] = useReducer(() => {
+        const query = new URLSearchParams(window.location.search);
+        const region = query.get("region");
+        return regionRegExp.test(region)? region : "original";
+    }, "original");
+
+    useEffect(() => {
+        setSelectedRegion();
+    });
+
+    const isOriginal = region == null || region === "original";
+
     const headings = Object.keys(labels).map(label => {
         const labels = {
             swsh: "SwSh",
@@ -84,28 +144,62 @@ function TransferabilityTable({pokemon, labels, index}) {
             Yes: <span><b>✓</b> Yes</span>,
             No: <span><b>✗</b> No</span>,
             Unknown: <span><b>?</b> Unknown</span>,
+            Native: <span><b>✓</b> Native</span>,
             "N/A": "N/A"
         }
+        const regionRegExp = new RegExp(`-${region}$`);
         if(pokemon[label]) {
-            return (<Link key={label} href={`/${label}/${pokemon[latestGame].id}`}>
+            const defaultLabel = (<Link key={label} href={`/${label}/${pokemon[latestGame].id}${isOriginal?"":`?region=${region}`}`}>
                 <a className={classNames(passportStyles.gridRow,passportStyles[pokemon[label].status],passportStyles[label+"-"+(pokemon[label].status)])}>
                     {status[labels[label][pokemon[label].status]]}
                 </a>
-            </Link>);
+            </Link>)
+            if(label === "swsh") {
+                if(region === "original") {
+                    return defaultLabel;
+                } else {
+                    if(pokemon[latestGame].forms?.find(variant => regionRegExp.test(variant.id))) {
+                        if(region === "galar") {
+                            // Galarian Slowpoke was introduced in 1.1.0, and is classified as "Other" in this listing.
+                            if (index === 79)
+                                return <div key={label} className={classNames(passportStyles.gridRow,passportStyles["swsh-other"])}>{status.Native}</div>
+                            // Galarian Slowbro was introduced in Isle of Armor.
+                            if (index === 80)
+                                return <div key={label} className={classNames(passportStyles.gridRow,passportStyles["swsh-armor"])}>{status.Native}</div>
+                            // Galarian Articuno, Zapdos, Moltres and Slowking were introduced in Crown Tundra.
+                            else if((index >= 144 && index < 147) || index === 199)
+                                return <div key={label} className={classNames(passportStyles.gridRow,passportStyles["swsh-crown"])}>{status.Native}</div>
+                            else
+                                return <div key={label} className={classNames(passportStyles.gridRow,passportStyles["swsh-base"])}>{status.Native}</div>
+                        } else if(region === "alola") {
+                            if(pokemon.status === "no")
+                                return <div key={label} className={classNames(passportStyles.gridRow,passportStyles["no"])}>{status.No}</div> 
+                            else
+                                return <div key={label} className={classNames(passportStyles.gridRow,passportStyles["swsh-crown"])}>{status.Yes}</div>
+                        } else {
+                            return <div key={label} className={classNames(passportStyles.gridRow,passportStyles["not-available"])}>{status["N/A"]}</div>;
+                        }
+                    } else {
+                        return defaultLabel;
+                    }
+                }
+            } else if(label === "visc") {
+                return defaultLabel;
+            }
         } else {
             if(label === "swsh") {
                 if(index >= 810 && index < 891)
-                    return <div key={label} className={classNames(passportStyles.gridRow,passportStyles["swsh-base"])}><span><b>✓</b> Native</span></div>
+                    return <div key={label} className={classNames(passportStyles.gridRow,passportStyles["swsh-base"])}>{status.Native}</div>
                 else if (index >= 891 && index < 894)
-                    return <div key={label} className={classNames(passportStyles.gridRow,passportStyles["swsh-armor"])}><span><b>✓</b> Native</span></div>
+                    return <div key={label} className={classNames(passportStyles.gridRow,passportStyles["swsh-armor"])}>{status.Native}</div>
                 else if (index >= 894 && index < 899)
-                    return <div key={label} className={classNames(passportStyles.gridRow,passportStyles["swsh-crown"])}><span><b>✓</b> Native</span></div>
+                    return <div key={label} className={classNames(passportStyles.gridRow,passportStyles["swsh-crown"])}>{status.Native}</div>
                 else
-                    return <div key={label} className={classNames(passportStyles.gridRow,passportStyles["not-available"])}>N/A</div>
+                    return <div key={label} className={classNames(passportStyles.gridRow,passportStyles["not-available"])}>{status["N/A"]}</div>
             } else if(label === "arceus" && index >= 899 && index < 906) {
-                return <div key={label} className={classNames(passportStyles.gridRow,passportStyles["confirmed"])}><span><b>✓</b> Native</span></div>
+                return <div key={label} className={classNames(passportStyles.gridRow,passportStyles["confirmed"])}>{status.Native}</div>
             } else {
-                return <div key={label} className={classNames(passportStyles.gridRow,passportStyles["not-available"])}>N/A</div>
+                return <div key={label} className={classNames(passportStyles.gridRow,passportStyles["not-available"])}>{status["N/A"]}</div>
             }
         }
         
@@ -118,7 +212,7 @@ function TransferabilityTable({pokemon, labels, index}) {
     )
 }
 
-function NavigationLink({pokemon, number, direction}) {
+function NavigationLink({pokemon, number, direction, preferredForm}) {
     const directions = {
         left: "←",
         right: "→",
@@ -127,9 +221,24 @@ function NavigationLink({pokemon, number, direction}) {
         left: "First Pokémon",
         right: "Last Pokémon"
     }
+
+    const isOriginal = preferredForm==null || preferredForm==="original";
+
     if(pokemon!=null) {
         let thisPokemon = {...pokemon};
-        return (<Link href={`/passport/${pokemon.id}`} passHref>
+        let form = "original";
+        if(!isOriginal && pokemon.forms?.length > 0) {
+            thisPokemon = pokemon.forms.find(form => {
+                const formTest = new RegExp(`-(?:${preferredForm})`); 
+                return formTest.test(form.id);
+            });
+            if(thisPokemon)
+                form = preferredForm;
+            else
+                // Since we're not reassigning thisPokemon, we don't have to clone here.
+                thisPokemon = pokemon;
+        }
+        return (<Link href={`/passport/${pokemon.id}${form!=="original"?`?region=${form}`:""}`} passHref>
             <a className={styles.navPokemon}>
                 <div>
                     <img alt={thisPokemon.name} className={classNames(styles.navIcon, passportStyles.default)} src={`https://raw.githubusercontent.com/msikma/pokesprite/master/pokemon-gen8/regular/${thisPokemon.id}.png`}/>
--- pages/passport/index.js
+++ pages/passport/index.js
@@ -1,18 +1,53 @@
+import { useReducer, useEffect } from 'react';
 import Head from 'next/head'
 import Link from 'next/link'
-import classNames from 'classnames';
 
 import { addTrailingZeroes } from "../../util.ts";
 
 import tableStyles from "../../styles/Table.module.css";
 
-export default function PassportsTable({ pokemonList }) {
+const getRegion = () => {
+    const query = new URLSearchParams(window.location.search);
+    const region = query.get("region");
+    return regionRegExp.test(region)? region : "original";
+}
 
-    const tableCells = pokemonList.map(pokemon => { 
-        const shortenedId = pokemon.id;
-        return <Link key={pokemon.id} href={`/passport/${shortenedId}`}>
+const regionRegExp = /(alola|galar|hisui)/;
+
+export default function PassportsTable({ pokemonList }) {
+    const [selectedRegion, setSelectedRegion] = useReducer(() => getRegion(),"original");
+    useEffect(() => {
+        setSelectedRegion();
+    });
+
+    // Clone pokemonList to avoid mutating the original
+    const pokeList = [...pokemonList];
+    const pokemonWithForms = pokeList.filter(pokemon => pokemon.forms != null)
+
+    if(selectedRegion !== "original") {
+        for(const pokemonId in pokemonWithForms) {
+            const pokemon = pokemonWithForms[pokemonId];
+            const form = pokemon.forms.find(form => new RegExp(`-(?:${selectedRegion})`).test(form.id));
+            if(form != null) {
+                const originalPoke = pokemonList.find(poke => poke.id === pokemon.id);
+                pokeList[pokemonList.indexOf(originalPoke)] = form;
+            }
+        }
+    }
+
+    const tableCells = pokeList.map(pokemon => { 
+        let region = "original", shortenedId = pokemon.id;
+
+        if(regionRegExp) {
+            const test = regionRegExp.exec(pokemon.id);
+            region = test? test[1] : "original";
+            if(region!=="original")
+                shortenedId = pokemon.id.replace("-"+region, '');
+        }
+
+        return <Link key={pokemon.id} href={`/passport/${shortenedId}${region!=="original"?`?region=${region}`:""}`}>
             <a><div className={tableStyles.tableEntry} style={{backgroundColor: "#f5f1dc"}}>
-                <span className={tableStyles.tableEntryNumber}>{addTrailingZeroes(pokemonList.indexOf(pokemon)+1, 3)}</span>
+                <span className={tableStyles.tableEntryNumber}>{addTrailingZeroes(pokeList.indexOf(pokemon)+1, 3)}</span>
                 <img alt={pokemon.name} src={`https://raw.githubusercontent.com/msikma/pokesprite/master/pokemon-gen8/regular/${pokemon.id}.png`}/>
                 </div></a>
         </Link>
@@ -27,12 +62,40 @@ export default function PassportsTable({ pokemonList }) {
                 <h1>Explore Pokémon passports</h1>
                 <p>Here is the list of all Pokémon from all currently released generations.<br/>
                 You may check their individual passports by clicking on their entry.</p>
-                <p>Currently, regional forms are not shown in each Pokémon&apos;s passport.</p>
+                <p>You can switch between regional forms by clicking one of the regional emblems below.</p>
+                <RegionSelector/>
             </center>
         <div className={tableStyles.table}>
             {tableCells}
         </div>
     </div>)
+}
+
+function RegionSelector() {
+    const regions = [
+        {id: "original", label: "Original regions"},
+        {id: "alola", label: "Alola"},
+        {id: "galar", label: "Galar"},
+        {id: "hisui", label: "Hisui"},
+    ];
+    const images = {
+        original: "/poke-passport/logo-visc.svg",
+        alola: "/poke-passport/logo-alola.svg",
+        galar: "/poke-passport/logo-swsh.svg",
+        hisui: "/poke-passport/logo-arceus.svg"
+    }
+    return (
+        <nav className={tableStyles.regionNavigator}>
+            {regions.map(region => (
+                <Link key={region.id} href={`/passport/?region=${region.id}`} passHref>
+                    <div className={tableStyles.regionSelector}>
+                        <img alt={region.label} src={images[region.id]}/>
+                        <a>{region.label}</a>
+                    </div>
+                </Link>
+            ))}
+        </nav>
+    );
 }
 
 export async function getStaticProps() {
